1fba3a3c5f90723a9bf6c3ad9031f767d4bfb292
ttmx <tiago.sequeira.teles@gmail.com>
2020-05-04 00:52:54 +0100

    refreshed files and grabbed map

--- Cartography.c
+++ Cartography.c
@@ -268,7 +268,7 @@ bool adjacentParcels(Parcel a, Parcel b)
 
 /* CARTOGRAPHY -------------------------------------- */
 
-int loadCartography(String fileName, Cartography cartography)
+int loadCartography(String fileName, Cartography *cartography)
 {
 	FILE *f;
 	int i;
@@ -279,7 +279,7 @@ int loadCartography(String fileName, Cartography cartography)
 	if( n > MAX_PARCELS )
 		error("Demasiadas parcelas no ficheiro");
 	for( i = 0 ; i < n ; i++ ) {
-		cartography[i] = readParcel(f);
+		(*cartography)[i] = readParcel(f);
 	}
 	fclose(f);
 	return n;
@@ -291,7 +291,7 @@ static int findLast(Cartography cartography, int n, int j, Identification id)
 		if( !sameIdentification(cartography[j].identification, id, 3) )
 			return j-1;
 	}
-	return n;
+	return n-1;
 }
 
 void showCartography(Cartography cartography, int n)
